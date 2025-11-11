88a615e9fd00abca1e68673ef190a78eb8e2100d
mb192012 <mb192012@alumno.udb.edu.sv>
2025-11-11 10:15:06 -0600

    final con docker

--- src/main/java/com/easycheck/infrastructure/config/CorsResponseFilter.java
+++ src/main/java/com/easycheck/infrastructure/config/CorsResponseFilter.java
@@ -13,7 +13,9 @@ public class CorsResponseFilter implements ContainerResponseFilter {
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
 
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
+        //responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        //responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://192.242.3.30:5173");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, X-Requested-With");
